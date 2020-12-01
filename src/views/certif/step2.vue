ce558629e47d9f3a528e19d9951bf7adfa193dff
feng <934520217@qq.com>
2020-12-01 10:51:03 +0800

    实名认证 第二步

--- src/views/certif/step2.vue
+++ src/views/certif/step2.vue
@@ -5,7 +5,7 @@
       src="~@/assets/images/common/white_back@2x.png"
       @click="$router.go(-1)"
     />
-    <h2 class="header">认证中心</h2>
+    <h2 class="header">卡信息认证</h2>
     <div class="step">
       <img class="step_img" src="~@/assets/images/certif/step2/step@2x.png" />
     </div>
@@ -17,27 +17,10 @@
     <div class="content">
       <div class="card_line">
         <div class="card">
-          <img src="~@/assets/images/certif/step1/camera@2x.png" />
-          <span>身份证正面照</span>
-        </div>
-        <div class="card">
-          <img src="~@/assets/images/certif/step1/camera@2x.png" />
-          <span>身份证背面照</span>
+          <img src="~@/assets/images/certif/step2/plus@2x.png" />
+          <span>上传储蓄卡正面</span>
         </div>
       </div>
-      <div class="card_line">
-        <div class="card">
-          <img src="~@/assets/images/certif/step1/camera@2x.png" />
-          <span>手持身份证</span>
-        </div>
-      </div>
-      <div class="claim">证件要求：</div>
-      <p class="claim_desc">1.请上传有效身份证照片，非证件照片不予受理</p>
-      <p class="claim_desc">2.证件照必须是彩色原件电子版</p>
-      <p class="claim_desc">
-        3.证件照需完整、清晰、请勿逆光拍摄，否则审核不通过
-      </p>
-      <div class="info">信息采集</div>
       <div class="input_wrap">
         <span>证件姓名：</span>
         <input type="text" placeholder="请输入真实姓名" />
@@ -123,7 +106,7 @@ export default {
     color: #ffffff;
   }
   .content {
-    margin-top: 190px;
+    margin-top: 120px;
     width: 580px;
     display: flex;
     align-items: center;
@@ -133,50 +116,29 @@ export default {
       display: flex;
       align-items: center;
       justify-content: space-between;
-      & + .card_line {
-        margin-top: 40px;
-      }
       .card {
-        width: 267px;
-        height: 166px;
+        width: 600px;
+        height: 248px;
         display: flex;
         align-items: center;
         justify-content: center;
         flex-direction: column;
-        background: url('"~@/assets/images/certif/step1/card_bg@2x.png')
-          no-repeat;
-        background-size: 100% 100%;
+        background: #f5f5f5;
+        // background: url('"~@/assets/images/certif/step1/card_bg@2x.png')
+        //   no-repeat;
+        // background-size: 100% 100%;
         img {
-          width: 80px;
-          height: 80px;
+          width: 125px;
+          height: 125px;
+          border: dashed 1px #888888;
+          padding: 2px;
         }
         span {
-          margin-top: 10px;
-          font-size: 22px;
-          color: #999999;
+          margin-top: 20px;
+          font-size: 24px;
+          color: #333;
         }
       }
-    }
-    .claim {
-      margin-top: 58px;
-      width: 100%;
-      text-align: left;
-      font-size: 24px;
-      color: #333333;
-    }
-    .claim_desc {
-      width: 100%;
-      text-align: left;
-      margin-top: 20px;
-      font-size: 20px;
-      color: #999999;
-    }
-    .info {
-      width: 100%;
-      text-align: left;
-      margin-top: 38px;
-      font-size: 24px;
-      color: #333333;
     }
     .input_wrap {
       margin-top: 23px;

3b14075721cc2d56fd52ea89ab4ab5600bb54f6e
kamo <logen.mo@lixinchuxing.com>
2020-12-19 22:59:35 +0800

    gerg

--- src/rest/config.js
+++ src/rest/config.js
@@ -14,7 +14,7 @@ let baseUrl = '';
 if (process.env.NODE_ENV === 'production') {
   // baseUrl = 'http://ydtapi.kuaikuaifu.net/api';
   // baseUrl = 'http://192.168.0.155:9000';
-  baseUrl = 'http://pay.fuyungroup.com';
+  baseUrl = 'http://newpay.kuaikuaifu.net';
   // baseUrl = '.';
 }
 export default baseUrl;

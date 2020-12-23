57c0501b486c68790373b82f02fd84de9d260da0
kamo <logen.mo@lixinchuxing.com>
2020-12-23 16:23:29 +0800

    eger

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,9 @@ const addPlugins = [
     },
   ],
 ];
-if (process.env.NODE_ENV === 'production') {
-  addPlugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }]);
-}
+// if (process.env.NODE_ENV === 'production') {
+//   addPlugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }]);
+// }
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: addPlugins,
